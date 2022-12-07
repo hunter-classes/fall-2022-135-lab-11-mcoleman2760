5a742943920bb07edd06c47c2ab4d1557c201c45
mcoleman2760 <104951107+mcoleman2760@users.noreply.github.com>
2022-12-07 03:25:40 -0500

    Update tests.cpp

--- tests.cpp
+++ tests.cpp
@@ -1,1 +1,34 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include <iostream>
+#include "doctest.h"
+#include "profile.h"
+#include "network.h"
 
+
+
+TEST_CASE ("Profile Class"){
+
+Profile prof1("Mike", "michael");
+CHECK(prof1.getUsername() == "Mike");
+CHECK(prof1.getFullName() == "michael (@Mike)");
+prof1.setDisplayName("michael coleman");
+CHECK(prof1.getFullName() == "michael coleman (@Mike)");
+
+
+} 
+
+TEST_CASE ("Network Class") {
+    Network nw;
+    CHECK(nw.addUser("player1", "Jeff") == true);
+    CHECK(nw.addUser("player1", "Jeff") == false);
+    CHECK(nw.addUser("#play", "John") == false);
+    CHECK(nw.addUser("newcharacter", "Jake") == false);
+    CHECK(nw.addUser("lastplayer", "Jamal") == false);
+    
+    CHECK(nw.follow("player1", "newcharacter") == true);
+    CHECK(nw.follow("player1", "who") == false);
+    CHECK(nw.follow("player1" , "lastplayer") == true);
+
+
+
+}
